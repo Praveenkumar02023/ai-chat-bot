899cfd4cb1aa825018337d449b4572a9a9b9ee19
Praveen <Praveenkumar042023@gmail.com>
2026-02-09 14:28:50 +0530

    init.

---
+++ src/main/java/com/example/GemeniApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GemeniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GemeniApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/model/chatModel.java
@@ -0,0 +1,21 @@
+package com.example.model;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "chat")
+public class chatModel {
+    @Id
+    private String id;
+    private String prompt;
+    private String response;
+}
